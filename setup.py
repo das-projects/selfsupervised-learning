35ab59496a44d8aab0f6b69393115247a8ef49db
Arijit <arijitd@gmail.com>
2022-01-28 10:18:08 +0100

    fix image and labels

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
+    name='Self-supervised ',
     version='0.0.0',
     description='Describe Your Cool Project',
     author='',
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
+    url='https://github.com/das-projects/selfsupervised-learning',
     install_requires=['pytorch-lightning'],
     packages=find_packages(),
 )
